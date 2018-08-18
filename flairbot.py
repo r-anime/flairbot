e403678216993873b8d46a286c332e48609a7ed7
George <georgej1088@gmail.com>
2018-08-18 17:20:56 -0400

    Misc. changes to some stuff

--- flairbot.py
+++ flairbot.py
@@ -7,6 +7,7 @@ print("Reading config")
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+# someone who knows python please tell me there's a better way to do this
 subreddit = config["Options"]["subreddit"]
 interval = int(config["Options"]["interval"])
 reminder_age = int(config["Options"]["reminder_age"])
@@ -14,6 +15,10 @@ removal_age = int(config["Options"]["removal_age"])
 posts_per_run = int(config["Options"]["posts_per_run"])
 reminder_message = config["Options"]["reminder_message"]
 removal_message = config["Options"]["removal_message"]
+dry_run = config["Options"].getboolean("dry_run", False)
+
+if dry_run:
+	print("  Performing a dry run; no replies or removals will be made")
 
 # These values are exposed for use in formatted messages, but not directly used
 reminder_age_minutes = int(reminder_age / 60)
@@ -21,10 +26,11 @@ removal_age_minutes = int(removal_age / 60)
 
 r = praw.Reddit(**config["Auth"])
 print(f"Logged in as /u/{r.user.me().name}")
+# if not dry_run:
+# 	raise Exception
 sub = r.subreddit(subreddit)
 print(f"Watching /r/{sub.display_name}")
 
-print("Attempting to read last state")
 try:
 	with open("remindedIds.json", "r") as file:
 		remindedIds = json.load(file)
@@ -33,6 +39,35 @@ except:
 	print("Failed to recover state from last run, starting fresh")
 	remindedIds = []
 
+initial_time = time.time()
+
+def remind_to_add_flair(submission):
+	if dry_run:
+		print(f"    confirm: https://redd.it/{submission.id}")
+	else:
+		try:
+			submission.reply(reminder_message.format(
+				username=submission.author.name,
+				reminder_age_minutes=reminder_age_minutes,
+				removal_age_minutes=removal_age_minutes
+			))
+		except Exception as e:
+			print(f"    {e!r}")
+
+def remove_for_missing_flair(submission):
+	if dry_run:
+		print(f"    confirm: https://redd.it/{submission.id}")
+	else:
+		try:
+			submission.mod.remove()
+			submission.reply(removal_message.format(
+				username=submission.author.name,
+				reminder_age_minutes=reminder_age_minutes,
+				removal_age_minutes=removal_age_minutes
+			))
+		except Exception as e:
+			print(f"    {e!r}")
+
 def main():
 	print("Running")
 
@@ -40,38 +75,30 @@ def main():
 		post_age = int(time.time() - post.created_utc)
 
 		if post.link_flair_text or post.link_flair_css_class:
-			print(f"Skip    {post.id} (age={post_age}, link_flair_text={repr(post.link_flair_text)}, link_flair_css_class={repr(post.link_flair_css_class)})")
+			print(f"  Flaired {post.id} (link_flair_text={repr(post.link_flair_text)}, link_flair_css_class={repr(post.link_flair_css_class)})")
+
+		elif post.created_utc < initial_time:
+			print(f"  Too Old {post.id} (age={post_age})")
 
 		elif post_age > removal_age:
-			print(f"Remove  {post.id} (age={post_age})")
-
+			print(f"  Remove  {post.id} (age={post_age})")
+			remove_for_missing_flair(post)
 			# We don't need to track the ID anymore because it won't be in /new
 			if (post.id in remindedIds):
 				remindedIds.pop(remindedIds.index(post.id))
 
-			post.mod.remove()
-			post.reply(removal_message.format(
-				username=post.author.name,
-				reminder_age_minutes=reminder_age_minutes,
-				removalAgeMinutes=removalAgeMinutes
-			))
-
 		elif post_age > reminder_age and not post.id in remindedIds:
-			print(f"Remind  {post.id} (age={post_age})")
+			print(f"  Remind  {post.id} (age={post_age})")
+			remind_to_add_flair(post)
 			remindedIds.append(post.id)
 
-			post.reply(reminder_message.format(
-				username=post.author.name,
-				reminder_age_minutes=reminder_age_minutes,
-				removalAgeMinutes=removalAgeMinutes
-			))
-
 		else:
-			print(f"Wait    {post.id} (age={post_age}, reminded={post.id in remindedIds})")
+			print(f"  Wait    {post.id} (age={post_age}, reminded={post.id in remindedIds})")
 
 	# Remove post IDs we shouldn't encounter anymore
 	# TODO: this isn't perfect, older posts can backflow into /new when removing newer posts
-	while len(remindedIds) > posts_per_run:
+	# HACK: multiply by 3 to hopefully prevent any reasonable issues
+	while len(remindedIds) > 3 * posts_per_run:
 		remindedIds.pop(0)
 
 	# write IDs to disk so we can resume if a crash occurs
@@ -80,9 +107,9 @@ def main():
 
 	print("Finished")
 
-
-# https://stackoverflow.com/a/25251804
-start = time.time()
-while True:
-	main()
-	time.sleep(float(interval) - ((time.time() - start) % interval))
+if __name__ == "__main__":
+	# https://stackoverflow.com/a/25251804
+	start = time.time()
+	while True:
+		main()
+		time.sleep(float(interval) - ((time.time() - start) % interval))
